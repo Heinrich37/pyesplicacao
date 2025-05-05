375252cfb455cf334dc4d1d3e6689398a7261fe0
Heinrich37 <belforthenrich2004@gmail.com>
2025-05-05 19:23:13 -0400

    ..

--- While.py
+++ While.py
@@ -78,20 +78,73 @@
 
 #---------------------------------------------------------------------------
 
-while True:
-    nome = input("Infome seu nome:\n")
-    idade = int(input("Informe sua idade:\n"))
-    salario = float(input("Informe seu salario:\n"))
-    sexo = input("Informe seu sexo. (f)Para feminino (m)Para masculino (o)Para outros")
-    estado_civil = input("Informe seu estado civil. (s)Para solteiro(a), (c)Para casado(a), (v)Para viuvo(a), (d)Para divorciado(a)")
-    quantidade = len(nome)
-    if (nome >= 3 and idade):
-        
-        
- 
+# while True:
+#     nome = input("Infome seu nome:\n")
+#     idade = int(input("Informe sua idade:\n"))
+#     salario = float(input("Informe seu salario:\n"))
+#     sexo = input("Informe seu sexo. (f)Para feminino (m)Para masculino (o)Para outros")
+#     estado_civil = input("Informe seu estado civil. (s)Para solteiro(a), (c)Para casado(a), (v)Para viuvo(a), (d)Para divorciado(a)")
+#     quantidade = len(nome)
+#      if (nome >= 3 and idade):
 
 
 
 
+
+Bolsonaro = 0
+Lula = 0
+Geraldo = 0
+Ciro = 0
+nulo = 0
+branco = 0
+
+print("------------------------------Sistema de votação presidencial------------------------------")
+
+print("1 - Bolsonaro")
+print("2 - Lula")
+print("3 - Geraldo Alckmin")
+print("4 - Ciro gomes")
+print(" - Nulo")
+print("Voto em Branco")
+
+voto = -1
+
+while voto != 0:
+    voto = int(input("Digite numero do candidato: "))
+    
+    if voto == 1:
+        Bolsonaro += 1 
+    elif voto == 2:
+        Lula += 1
+    elif voto == 3:
+        Geraldo += 1
+    elif voto == 4:
+        Ciro += 1
+    elif voto == " ":
+        nulo += 1
+    elif voto == 6:
+        branco += 1
+    elif voto == 0:
+        print("Votaçao encerrada!!!")
+    else:
+        print ("Voto invalido! Tente novamente.")
     
     
+print ("------------------------------RESULTADO FINAL DA ELEIÇÃO------------------------------")
+print("Bolsonaro:", Bolsonaro, "Votos")
+print("Lula:", Lula, "Votos")
+print("Geraldo :", Geraldo, "Votos")
+print("Ciro:", Ciro, "Votos")
+print("Nulo:", nulo, "Votos")
+print("Branco:", branco, "Votos")
+
+if Lula > Bolsonaro and Lula > Geraldo and Lula > Ciro:
+    print("Lula ganhador")
+if Bolsonaro > Lula and Bolsonaro > Geraldo and Bolsonaro > Ciro:
+    print("2 ganhador")
+if Geraldo > Lula and Geraldo > Bolsonaro and Geraldo > Ciro:
+    print("Geraldo ganhador")
+if Ciro > Lula and Ciro > Geraldo and Ciro > Bolsonaro:
+    print("Ciro ganhador")
+
+
